ee8ecbec294c7ccceaccf06c3c29dcea6cacd4cf
Marco Montanari <sirmmo@gmail.com>
2017-06-06 19:27:07 +0000

    update

--- core/admin.py
+++ core/admin.py
@@ -5,3 +5,6 @@ from .models import *
 admin.site.register(UrlMode)
 admin.site.register(Application)
 admin.site.register(AppConfiguration)
+admin.site.register(ClaimType)
+admin.site.register(AppClaim)
+admin.site.register(UserClaim)
